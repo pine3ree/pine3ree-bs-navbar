7a395533c265869ef12df8df22227cec4fe0f03d
pine3ree <pine3ree@gmail.com>
2023-07-20 01:57:05 +0200

    Use twbs browserslistrc configuration
    Signed-off-by: pine3ree <pine3ree@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -74,9 +74,15 @@ const NANO_OPTIONS = {
 const AUTOPREFIXER_OPTIONS = {
     remove: false,
     overrideBrowserslist: [
-        ">= 0.2% or last 2 major versions",
+        ">= 0.5%",
+        "last 2 major versions",
         "not dead",
+        "Chrome >= 60",
+        "Firefox >= 60",
         "Firefox ESR",
+        "iOS >= 12",
+        "Safari >= 12",
+        "not Explorer <= 11",
     ]
 };
 
